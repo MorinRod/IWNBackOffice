bed0b6afc472f65295d669867c3b6c1e7cc3d78e
Morin <morinrod@gmail.com>
2017-05-18 12:49:00 +1000

    New design and edit contact fix

--- client/src/app/components/ContactComponent.ts
+++ client/src/app/components/ContactComponent.ts
@@ -7,20 +7,20 @@ import {Member} from "../models/Member";
   selector: 'contact-component',
   template: `
 <!--<div class="col-xs-1  hidden-xs" [appHighlight]="hilightWord">&nbsp;{{index + 1}}</div>-->
-<div class="col-xs-1" [innerHTML]="contact.firstName | hilight:hilightWord"></div>
-<div class="col-xs-1" [innerHTML]="contact.lastName | hilight:hilightWord"></div>
-<div class="col-xs-1"  [innerHTML]="contact.idNumber | hilight:hilightWord"></div>
-<div class="col-xs-1 hidden-xs" [innerHTML]="contact.phoneNumber | hilight:hilightWord"></div>
-<div class="col-xs-1 hidden-xs"  [innerHTML]="contact.city | hilight:hilightWord"></div>
-<div class="col-xs-1 hidden-xs"  [innerHTML]="contact.address | hilight:hilightWord"></div>
-<div class="col-xs-2" [innerHTML]="contact.emailAddress | hilight:hilightWord"></div>
-<div class="col-xs-1 hidden-xs">&nbsp;{{contact.fromDate}}</div>
-<div class="col-xs-1 hidden-xs" >{{contact.toDate | date:'dd/MM/yy'}}</div>
+<div class="col-md-1" [innerHTML]="contact.firstName | hilight:hilightWord"></div>
+<div class="col-md-1" [innerHTML]="contact.lastName | hilight:hilightWord"></div>
+<div class="col-md-1"  [innerHTML]="contact.idNumber | hilight:hilightWord"></div>
+<div class="col-md-1 hidden-xs" [innerHTML]="contact.phoneNumber | hilight:hilightWord"></div>
+<div class="col-md-1 hidden-xs"  [innerHTML]="contact.city | hilight:hilightWord"></div>
+<div class="col-md-1 hidden-xs"  [innerHTML]="contact.address | hilight:hilightWord"></div>
+<div class="col-md-2" [innerHTML]="contact.emailAddress | hilight:hilightWord"></div>
+<div class="col-md-1 hidden-xs">&nbsp;{{contact.fromDate}}</div>
+<div class="col-md-1 hidden-xs" >{{contact.toDate | date:'dd/MM/yy'}}</div>
 
-<div class="col-xs-1"><a [routerLink]="['/payment', contact.idNumber]">תשלומים</a></div>
+<div class="col-md-1"><a [routerLink]="['/payment', contact.idNumber]">תשלומים</a></div>
 
-<!--<div class="col-xs-1" [innerHTML]="contact.wantUpdates | booleanpipe"></div>-->
-<!--<div class="col-xs-1" [innerHTML]="contact.member | booleanpipe"></div>-->
+<!--<div class="col-md-1" [innerHTML]="contact.wantUpdates | booleanpipe"></div>-->
+<!--<div class="col-md-1" [innerHTML]="contact.member | booleanpipe"></div>-->
 `
 })
 export class ContactComponent {
